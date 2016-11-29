0774f7041cdd009fa69cb0563548489a97462262
Victor Barros <victor@kinvey.com>
2016-11-28 18:31:20 -0800

    Adding a sanity test for Carthage

---
+++ SanityTest/Carthage/KinveyCarthageTest-Develop/KinveyCarthageTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KinveyCarthageTest
+//
+//  Created by Victor Hugo on 2016-11-28.
+//  Copyright © 2016 Kinvey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
